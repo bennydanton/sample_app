e3b42e21b7e1ce626523719b8dadf6dde81c5337
Daniel Benton <dannybenton@gmail.com>
2014-08-01 14:54:54 -0500

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,8 +1,7 @@
 require 'spec_helper'
 
 describe "Static pages" do
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
-  
+
   subject { page }
 
   describe "Home page" do
@@ -15,23 +14,22 @@ describe "Static pages" do
 
   describe "Help page" do
     before { visit help_path }
-    
+
     it { should have_content('Help') }
     it { should have_title(full_title('Help')) }
   end
-    
 
   describe "About page" do
     before { visit about_path }
-    
+
     it { should have_content('About') }
     it { should have_title(full_title('About Us')) }
-  end  
+  end
 
   describe "Contact page" do
     before { visit contact_path }
-      
-    it { should have_content('Contact Us') }
-    it { should have_title(full_title('Contact Us')) }
+
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 end
